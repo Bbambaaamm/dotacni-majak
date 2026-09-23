f4411263c35dc41b466e91b85683d4174fc67b97
Bbambaaamm <bartmisk8@gmail.com>
2026-09-23 19:30:46 +0200

    web: render source suggestion route

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -4,6 +4,7 @@ import { ComparePage } from "./pages/ComparePage";
 import { GrantDetailPage } from "./pages/GrantDetailPage";
 import { HomePage } from "./pages/HomePage";
 import { SearchPage } from "./pages/SearchPage";
+import { SuggestSourcePage } from "./pages/SuggestSourcePage";
 import { resolveRoute } from "./lib/routes";
 
 function PlaceholderPage({ title }: { title: string }) {
@@ -46,6 +47,9 @@ export function App() {
       break;
     case "projects":
       content = <PlaceholderPage title="Moje projekty" />;
+      break;
+    case "suggest-source":
+      content = <SuggestSourcePage />;
       break;
     default:
       content = <NotFound />;
