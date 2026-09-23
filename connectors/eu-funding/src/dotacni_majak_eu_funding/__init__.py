aec0ac159b4df54541dd57ed29579652634e7bd6
Bbambaaamm <bartmisk8@gmail.com>
2026-09-23 10:18:26 +0200

    eu-funding: add connectors/eu-funding/src/dotacni_majak_eu_funding/__init__.py

---
+++ connectors/eu-funding/src/dotacni_majak_eu_funding/__init__.py
@@ -0,0 +1,13 @@
+from .adapter import (
+    EuFundingTendersAdapter,
+    STATUS_CLOSED,
+    STATUS_FORTHCOMING,
+    STATUS_OPEN,
+)
+
+__all__ = [
+    "EuFundingTendersAdapter",
+    "STATUS_CLOSED",
+    "STATUS_FORTHCOMING",
+    "STATUS_OPEN",
+]
