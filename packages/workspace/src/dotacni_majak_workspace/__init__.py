4b5c63e2cccb513dfec6305fe42695c67c868462
Bbambaaamm <bartmisk8@gmail.com>
2026-09-23 13:09:05 +0200

    workspace: add packages/workspace/src/dotacni_majak_workspace/__init__.py

---
+++ packages/workspace/src/dotacni_majak_workspace/__init__.py
@@ -0,0 +1,25 @@
+from .engine import (
+    ApplicationWorkspace,
+    GrantRequirementInput,
+    ReadinessState,
+    ReadinessSummary,
+    RequirementNecessity,
+    WorkspaceEngine,
+    WorkspaceStatus,
+    WorkspaceTask,
+    WorkspaceTaskSource,
+    WorkspaceTaskStatus,
+)
+
+__all__ = [
+    "ApplicationWorkspace",
+    "GrantRequirementInput",
+    "ReadinessState",
+    "ReadinessSummary",
+    "RequirementNecessity",
+    "WorkspaceEngine",
+    "WorkspaceStatus",
+    "WorkspaceTask",
+    "WorkspaceTaskSource",
+    "WorkspaceTaskStatus",
+]
